0c77a9638528dadc81c4d50a76ef09586d525978
Philip Washington Sorst <philip@sorst.net>
2017-08-27 22:51:29 +0200

    Removing Scheduling

--- src/main/java/net/dontdrinkandroot/stack/wicket/Application.java
+++ src/main/java/net/dontdrinkandroot/stack/wicket/Application.java
@@ -4,13 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.ErrorMvcAutoConfiguration;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * @author Philip Washington Sorst <philip@sorst.net>
  */
 @SpringBootApplication
-@EnableScheduling
 @EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class})
 public class Application
 {
